0b43f8ebe8c1d134c9a1c30a4afa50af55a2cb52
XadillaX <admin@xcoder.in>
2014-05-11 18:26:02 +0800

    del mongodb.js

--- config/mongodb.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by XadillaX on 14-1-23.
- */
-module.exports = {
-    hostname        : "localhost",
-    port            : 27017,
-    dbname          : "exframess",
-    poolSize        : 4
-};
